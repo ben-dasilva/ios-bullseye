9d60a80bfa48fb86ffa53f7f397fa8058d410dbb
Ben da Silva <d123097@gmail.com>
2016-12-29 20:46:52 -0500

    First changes.

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Ben da Silva on 12/28/16.
+//  Copyright © 2016 Ben da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
+
